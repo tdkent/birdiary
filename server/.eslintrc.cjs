237b0e724caa00e0664b17cdbb1408957990af34
Tim Kent <timkentdev@gmail.com>
2024-01-23 21:06:48 -0800

    add, configure eslint

---
+++ server/.eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+module.exports = {
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended-type-checked',
+	],
+	overrides: [
+		{
+			extends: ['plugin:@typescript-eslint/disable-type-checked'],
+			files: ['./**/*.cjs'],
+		},
+	],
+	plugins: ['@typescript-eslint'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		project: ['./tsconfig.json'],
+		tsconfigRootDir: __dirname,
+	},
+	root: true,
+};
--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,4 @@
-import { app } from "./src/app.js";
+import { app } from './src/app.js';
 
 var PORT = 4000;
 
