2d44dc6e1f3cb034e2fcf5e3bef4e1f88d162245
Tim Kent <timkentdev@gmail.com>
2024-01-25 21:56:27 -0800

    add client trpc pkg, basic setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+import { client } from "./main";
 
 import "./App.css";
 
 function App() {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log("Attempting to fetch data");
+    const greeting = async () => {
+      const result = await client.get.query();
+      console.log("Result: ", result);
+      // setMessage(result);
+    };
+    greeting();
+  }, []);
 
   const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
     setText(e.currentTarget.value);
