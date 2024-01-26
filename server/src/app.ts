53036a685b44794e42f74b5cb39714542513896d
Tim Kent <timkentdev@gmail.com>
2024-01-25 21:32:37 -0800

    add nodemon, dev script

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,5 +3,5 @@ import express, { Request, Response } from 'express';
 export const app = express();
 
 app.use('/', (req: Request, res: Response) => {
-	res.json({ message: 'Hello world!' });
+	res.json({ message: 'Hello world!!!' });
 });
