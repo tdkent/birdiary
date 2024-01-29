cd2d965dbe32194014eb69d665f457dd242eab78
Tim Kent <timkentdev@gmail.com>
2024-01-28 17:37:04 -0800

    remove vite boilerplate. add root route.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,54 +1,30 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import { client } from "./main";
 
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import "./App.css";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 function App() {
-  const [text, setText] = useState("");
-  const [todos, setTodos] = useState<string[]>([]);
-
+  // trpc test
   useEffect(() => {
     console.log("Attempting to fetch data");
     const greeting = async () => {
       const result = await client.get.query();
-      console.log("Result: ", result);
-      // setMessage(result);
+      console.log("trpc test: ", result);
     };
     greeting();
   }, []);
 
-  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
-    setText(e.currentTarget.value);
-  };
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    setTodos((prev) => [...prev, text]);
-    setText("");
-  };
-
   return (
-    <main>
-      <Header />
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="">New Todo:</label>
-        <input value={text} type="text" onChange={handleChange} />
-        <button>Add</button>
-      </form>
-      <div>
-        {todos.map((t) => {
-          return (
-            <div key={t} title="todo-div">
-              {t}
-            </div>
-          );
-        })}
-      </div>
-      <Footer />
-    </main>
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
   );
 }
 
---
+++ client/src/layouts/Main.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+export default function Main() {
+  return (
+    <main>
+      <Header />
+      <div>Body of application</div>
+      <Footer />
+    </main>
+  );
+}
