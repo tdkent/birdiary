e50927ab4b0478cabf88be3cef1ec391704442e5
Tim Kent <timdekent@gmail.com>
2024-01-29 16:40:52 -0800

    config ts-router with docs examples

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createRouter } from '@tanstack/react-router';
 
-import App from './App.tsx';
+import { routeTree } from './routeTree.gen.ts';
+// import App from './App.tsx';
+
+const router = createRouter({ routeTree });
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
