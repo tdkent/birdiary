28d2207fe2b94c037726e6264bf1c96446234c1d
Tim Kent <timkentdev@gmail.com>
2024-01-29 22:42:32 -0800

    add basic router structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,76 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import Layout from '@layouts/Layout';
+
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello World!</div>
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <div>HOME PAGE</div>
+      },
+      {
+        path: 'diary',
+        children: [
+          {
+            index: true,
+            element: <div>DIARY PAGE INDEX</div>
+          },
+          {
+            path: ':diaryId',
+            element: <div>DIARY DATE: 1/1/2024</div>
+          }
+        ]
+      },
+      {
+        path: 'birds',
+        children: [
+          {
+            index: true,
+            element: <div>BIRDS PAGE INDEX</div>
+          },
+          {
+            path: ':birdId',
+            element: <div>BLUE JAY</div>
+          }
+        ]
+      },
+      {
+        path: 'locations',
+        children: [
+          {
+            index: true,
+            element: <div>LOCATIONS PAGE INDEX</div>
+          },
+          {
+            path: ':locationId',
+            element: <div>COYOTE HILLS REGIONAL PARK</div>
+          }
+        ]
+      },
+      {
+        path: 'profiles/:userId',
+        element: <div>PROFILE PAGE: tdkent</div>
+      },
+      {
+        path: 'profiles/:userId/edit',
+        element: <div>EDIT PROFILE: tdkent</div>
+      },
+      {
+        path: '/account',
+        element: <div>Update Account Settings</div>
+      },
+      {
+        path: '/login',
+        element: <div>LOGIN</div>
+      },
+      {
+        path: '/register',
+        element: <div>REGISTER</div>
+      }
+    ]
   }
 ]);
 
---
+++ client/src/layouts/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+
+export default function Layout() {
+  return (
+    <div>
+      <header>Header</header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>Footer</footer>
+    </div>
+  );
+}
